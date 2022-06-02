5594574b25d38a922c540ed4c770e78b95955409
Sebastian <bastianonofrei@gmail.com>
2022-06-02 04:17:19 +0300

    changed something i dont remember

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,27 @@ import './Header.css'
 import {AppBar, Toolbar,Typography,Avatar,IconButton} from '@mui/material'
 import TheDrawer from './TheDrawer'
 import SchoolTwoToneIcon from '@mui/icons-material/SchoolTwoTone';
-
+import ScrollIndicator from './ScrollIndicator';
 
 export default function Header() {
   return (
+    <React.Fragment>
+
+   
     <AppBar position='fixed' color='primary' className='theAppBar'>
+      
       <Toolbar>
         <TheDrawer/>
-         
+       
         <Typography variant='h3' textAlign='center'  id='brand'>
         <IconButton  color='inherit' disableRipple disableTouchRipple disableFocusRipple> <SchoolTwoToneIcon sx={{fontSize:'3em'}} /></IconButton>Java Educational Platform
     </Typography>
         <Avatar src='https://randomuser.me/api/portraits/men/35.jpg' className='myAvatar' size='small'>
         </Avatar>
       </Toolbar>
+      
     </AppBar>
+     <ScrollIndicator/>
+    </React.Fragment>
   )
 }
